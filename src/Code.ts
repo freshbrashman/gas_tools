7789d0bdcb034b552d72d985de1e9db2ed72c5fc
freshbrashman <fresh.brash.sarariiman@gmail.com>
2019-07-05 01:06:28 +0900

    スプレッドシートに買ったものを入力するとLINE通知する処理を実装！

---
+++ src/Code.ts
@@ -0,0 +1,37 @@
+const MANAGE_SHEET = "★いじっちゃダメ★";
+const SHOPPING_LIST_SHEET = "お買い物リスト";
+
+function onEdit(e: GoogleAppsScript.Events.SheetsOnEdit) {
+	// お買物シート以外の編集は無視
+	if(e.source.getSheetName() != SHOPPING_LIST_SHEET) {
+		return;
+	}
+
+	// Line通知トークンの取得
+	const manageSheet = SpreadsheetApp.getActiveSpreadsheet()
+													.getSheetByName(MANAGE_SHEET);
+	const token = manageSheet.getRange(1, 2).getValue().toString();
+
+	// 編集行の取得
+	const editedSheet = e.source.getActiveSheet();
+	const editedRow = e.range.getRow();
+
+	const editedYear = editedSheet.getRange(editedRow, 1).getValue().toString();
+	const editedDay = editedSheet.getRange(editedRow, 2).getValue().toString();
+	const editedProductName = editedSheet.getRange(editedRow, 3).getValue().toString();
+	const editedPayment = editedSheet.getRange(editedRow, 4).getValue().toString();
+
+	// 編集行の何かしらがまだ埋まっていない場合、通知はしないで終了
+	if(!editedYear || !editedDay || !editedProductName || !editedPayment) {
+		return;
+	}
+
+	const content = `お買い物リストに追加または変更されました。\n${editedYear}年${editedDay} ${editedProductName} ${editedPayment}`
+
+	const options: GoogleAppsScript.URL_Fetch.URLFetchRequestOptions = {
+		"method"  : "post",
+		"payload" : {"message": content,}, 
+		"headers" : {"Authorization" : "Bearer "+ token}
+	};
+	UrlFetchApp.fetch("https://notify-api.line.me/api/notify", options);
+}
