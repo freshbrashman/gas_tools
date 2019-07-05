c48dce78ea22f40af0220f13bdfe4c9b851520d8
freshbrashman <fresh.brash.sarariiman@gmail.com>
2019-07-06 00:05:27 +0900

    編集した行のセルに紐づくURLをLINE通知に含めるようにした

--- src/Code.ts
+++ src/Code.ts
@@ -12,22 +12,28 @@ function onEdit(e: GoogleAppsScript.Events.SheetsOnEdit) {
 													.getSheetByName(MANAGE_SHEET);
 	const token = manageSheet.getRange(1, 2).getValue().toString();
 
-	// 編集行の取得
+	// 編集した行の取得
 	const editedSheet = e.source.getActiveSheet();
 	const editedRow = e.range.getRow();
 
+	// 編集した行の各セル値取得
 	const editedYear = editedSheet.getRange(editedRow, 1).getDisplayValue();
 	const editedDay = editedSheet.getRange(editedRow, 2).getDisplayValue()
 	const editedProductName = editedSheet.getRange(editedRow, 3).getDisplayValue();
 	const editedPayment = editedSheet.getRange(editedRow, 4).getDisplayValue();
+
+	// 編集した行に紐づくURLを取得する
+	const editedRowUrl = SpreadsheetApp.getActiveSpreadsheet().getUrl() +
+													`#gid=0&range=${editedSheet.getRange(editedRow, 1, 1, 4).getA1Notation()}`
 
 	// 編集行の何かしらがまだ埋まっていない場合、通知はしないで終了
 	if(!editedYear || !editedDay || !editedProductName || !editedPayment) {
 		return;
 	}
 	const newOrDelete = e.oldValue?"編集":"追加";
-
-	const content = `\nお買い物リストが【${newOrDelete}】された。\n${editedRow}行目 ${editedYear}/${editedDay} ${editedProductName} ${editedPayment}`
+	const content = `\nお買い物リストが【${newOrDelete}】されました。\n` +
+									`${editedRow}行目 ${editedYear}/${editedDay} ${editedProductName} ${editedPayment}\n` +
+									`${editedRowUrl}`
 
 	const options: GoogleAppsScript.URL_Fetch.URLFetchRequestOptions = {
 		"method"  : "post",
